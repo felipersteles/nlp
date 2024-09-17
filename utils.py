f8730f2050368e52e49dc6af770fd0c394d87fe3
Felipe Teles <felipersteles@gmail.com>
2024-09-17 18:10:19 -0300

    add: stemming

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from nltk.corpus import stopwords
+
 def print_tokens_count(tokens):
     print("--------------------------------")
     print(f'Total of tokens: %s' % len(tokens))
@@ -25,3 +27,24 @@ def print_words_frequency(frequent_words):
     print("Frequency of words:")
     for word in frequent_words:
         print(word[0] + " - " + str(word[1]))
+
+def print_bar():
+    print("--------------------------------")
+
+def only_words(tokens):
+    words = []
+    for token in tokens:
+        if(token.isalpha()):
+            words.append(token)
+
+    return words
+
+def remove_stopwords(tokens):
+    stop_words = set(stopwords.words('english'))
+    filtered_words = []
+    
+    for token in tokens:
+        if(not token in stop_words):
+            filtered_words.append(token)
+
+    return filtered_words
